73001e276d6c6c6b940b5f2f13cbcacd452644dd
Guilherme Dias Ferreira <guilhermedias.ferreira@hotmail.com>
2023-03-22 15:42:18 -0300

    usando relative locator

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -2,9 +2,11 @@ package ui;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,7 +19,7 @@ public class LoginTest {
 	  
 	 */
 	
-	public static String browser = "Firefox";
+	public static String browser = "Chrome";
 	public static WebDriver driver;
 
 	public static void main(String[] args) {
@@ -33,18 +35,26 @@ public class LoginTest {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		}
-
 		
+//		Relative locator: são úteis quando não é fácil construir um localizador para o elemento desejado, 
+//		mas é fácil descrever espacialmente onde o elemento está em relação a um elemento que possui um localizador facilmente construído
+		
+		driver.get("https://www.saucedemo.com/");
+		WebElement password = driver.findElement(By.id("password"));
+		By username = RelativeLocator.with(By.tagName("input")).above(password);
+		driver.findElement(username).sendKeys("Testing");
+//
+				
 //		Abrindo o navegador
 //		ChromeDriver driver = new ChromeDriver(options);
-		driver.get("https://www.saucedemo.com/");
-		
-//		Preenchendo os inputs
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-		
-//		Clicando no botão de "Login"
-		driver.findElement(By.id("login-button")).click();
+//		driver.get("https://www.saucedemo.com/");
+//		
+////		Preenchendo os inputs
+//		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+//		
+////		Clicando no botão de "Login"
+//		driver.findElement(By.id("login-button")).click();
 		
 //		Fechar o navegador
 		driver.close();
